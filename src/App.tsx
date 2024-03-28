7b537de1651258e976312d69564e3626f458c5d0
khols <khols@gmail.com>
2024-03-28 13:22:50 +0800

    * update config

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import {TonProofDemo} from "./components/TonProofDemo/TonProofDemo";
 function App() {
   return (
       <TonConnectUIProvider
-          manifestUrl="https://ton-connect.github.io/demo-dapp-with-react-ui/tonconnect-manifest.json"
+          manifestUrl="https://skyeden.github.io/demo-dapp/tonconnect-manifest.json"
           uiPreferences={{ theme: THEME.DARK }}
           walletsListConfiguration={{
             includeWallets: [
